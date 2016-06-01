92885db6df89dc8f64983e121c097527f604e906
Lauren Posey and Sam Park <sam@>
2016-06-01 10:00:41 -0700

    it will function as expected

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("#letter1").submit(function(event) {
+    var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var animalInput= $("input#animal").val();
+    var exclamationInput = $("input#exclamation").val();
+    var verbInput = $("input#verb").val();
+    var nounInput = $("input#noun").val();
+    var userInput = $("input#userName").val();
+
+    $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".animal").text(animalInput);
+    $(".exclamation").text(exclamationInput);
+    $(".verb").text(verbInput);
+    $(".noun").text(nounInput);
+    $(".userName").text(userInput);
+
+    $("#story").show();
+    $("#letter").show();
+
+    event.preventDefault();
+  });
+});
